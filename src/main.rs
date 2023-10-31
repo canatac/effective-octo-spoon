83a7590717431c4ed5f140e48ff0ba77c4cfcfb5
Jan ATAC <can.atac@gmail.com>
2023-10-31 16:41:27 +0000

    clean unused import

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::process::{Command,Stdio};
 use std::env;
-use std::str;
-use chrono::NaiveDateTime;
 use ansi_term::Colour;
 use certeef::check_expiration_date_of;
-
 
 fn main() {
     
     let args: Vec<String> = env::args().collect();
     let url = &args[2];
-
 
     println!("{} expires in {} days", Colour::Yellow.bold().paint(url), Colour::Yellow.bold().paint(check_expiration_date_of(url).to_string()));
    
